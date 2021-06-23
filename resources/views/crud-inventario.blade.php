7687bcbd5885c3ec9e2ba915697d2e4635c9b1d5
Jeshmom <jesus15577@gmail.com>
2021-06-23 10:29:57 -0400

    puto git y livewire

--- resources/views/crud-inventario.blade.php
+++ resources/views/crud-inventario.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <livewire:admin-product>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    
     @livewireStyles
 </head>
 <body>
@@ -87,8 +87,7 @@
         </main>
     </div>
 
-
-    @livewireScripts
+    @livewireScripts 
 
 </body>
 </html>
