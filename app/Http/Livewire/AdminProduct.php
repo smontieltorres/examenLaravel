9d7db3d8cb5a42e01dd58eae60413d0f1293237b
Sebastian Montiel <Smontieltorres@gmail.com>
2021-06-23 09:40:33 -0400

    Instalacion de Livewire

---
+++ app/Http/Livewire/AdminProduct.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Product;
+
+class AdminProduct extends Component
+{
+
+    public $name, $stock, $price, $isEdit=false, $productID;
+
+    protected $rules = [
+        'name'=> 'required',
+        'stock' => 'required',
+        'price' => 'required'
+    ];
+
+    public function render()
+    {
+        $products = Product::orderBy('id',"DESC")->get();
+        return view('livewire.admin-product', compact('products'));
+    }
+
+    public function create(){
+
+        $this->validate();
+
+        Product::create([
+            'name'=>$this->name,
+            'stock'=>$this->stock,
+            'price'=>$this->price,
+        ]);
+    }
+
+    public function destroy($id){
+
+        $product = Product::find($id);
+
+        $product->delete();
+    }
+
+    public function default(){
+
+        $this->resetErrorBag();
+
+        $this->name = "";
+        $this->stock = "";
+        $this->price = "";
+        
+        if($this->isEdit == true){
+            $this->isEdit = false;
+        }
+    }
+
+    public function update($id){
+
+        $this->productID = $id;
+        $product = Product::find($id);
+
+        $this->isEdit = true;
+
+        $this->resetErrorBag();
+
+        $this->name = $product->name;
+        $this->stock = $product->stock;
+        $this->price = $product->price;
+    }
+
+    public function save(){
+        $product = Product::find($this->productID);
+    }
+}
---
+++ app/Http/Livewire/ShowProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Product;
+
+class ShowProduct extends Component
+{
+    public function render()
+    {
+        $products = Product::orderBy('id',"DESC")->get();
+
+        return view('livewire.show-product', compact('products'));
+    }
+}
